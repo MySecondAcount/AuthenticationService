ac128d5569c907f5a9306d075d54e4fcec844326
yazn <yaznyousef94@gmail.com>
2023-02-24 23:25:06 +0300

    first commit

---
+++ src/main/java/com/atypon/authenticationservice/AuthenticationServiceController.java
@@ -0,0 +1,22 @@
+package com.atypon.authenticationservice;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class AuthenticationServiceController {
+    @PostMapping("/login")
+    public boolean authenticate(@RequestBody UserInfo userInfo) {
+
+
+        if (userInfo.getEmail().equals("admin1@gmail.com") && userInfo.getPassword().equals("123456")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
